2bd18d33aacd4b5876f317ba2a80e7ec58f15b3c
Uma Mahesh <umamahesh@Umas-MacBook-Air.local>
2025-04-21 03:48:59 +0530

    ok

--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "../Home/Home";
 import Teams from "../Teams/Teams";
 import Projects from "../Projects/Projects";
 import About from "../About/About";
 import ServicesPage from "../ServicesPage/ServicesPage";
 
+// ScrollToTop component to handle scroll restoration
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function Routing(){
   return (
     <>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/teams" element={<Teams/>}/>
